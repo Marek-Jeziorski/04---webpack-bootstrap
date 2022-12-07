59f3dc5fb373bf8999bb34c2bfc5aedb959a63f4
Marek-Jeziorski <111072844+Marek-Jeziorski@users.noreply.github.com>
2022-12-07 14:59:36 +0100

    bootstrap added - RM !
    added whole bootstrap
    style.css
    style.scss

--- src/assets/scripts/App.js
+++ src/assets/scripts/App.js
@@ -1,4 +1,14 @@
+import "../styles/styles.scss";
 import "../styles/styles.css";
+
+// Import all of Bootstrap's JS
+import * as bootstrap from "bootstrap";
+
+// import Alert from 'bootstrap/js/dist/alert'
+
+// or, specify which plugins you need:
+
+// import { Tooltip, Toast, Popover } from 'bootstrap'
 
 let measure;
 
--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,30 @@ let cssConfig = {
     },
   ],
 };
+
+let scssConfig = {
+  test: /\.(scss)$/,
+  use: [
+    {
+      loader: "style-loader",
+    },
+    {
+      loader: "css-loader",
+    },
+    {
+      loader: "postcss-loader",
+      options: {
+        postcssOptions: {
+          plugins: () => [require("autoprefixer")],
+        },
+      },
+    },
+    {
+      loader: "sass-loader",
+    },
+  ],
+};
+
 let imagesConfig = {
   test: /\.(png|svg|jpg|jpeg|gif)$/i,
   type: "asset/resource",
@@ -64,7 +88,7 @@ let config = {
   devtool: "inline-source-map",
 
   module: {
-    rules: [cssConfig, imagesConfig],
+    rules: [cssConfig, scssConfig, imagesConfig],
   },
 
   plugins: pages,
